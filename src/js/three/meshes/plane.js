271e50a0787330baa243b1fad2388ae85b6767df
wind-of <adequation@mail.ru>
2023-03-09 17:23:22 +0300

    refactor: выделил planeMesh из horizontalPlaneMesh

--- src/js/three/meshes/plane.js
+++ src/js/three/meshes/plane.js
@@ -1,10 +1,15 @@
 import * as THREE from "three"
 
-export function horizontalPlaneMesh({ height, width, material }) {
+export function planeMesh({ height, width, material }) {
   const mesh = new THREE.Mesh(
     new THREE.PlaneGeometry(width, height, width, height),
     new THREE.MeshBasicMaterial(material)
   )
+  return mesh
+}
+
+export function horizontalPlaneMesh(options) {
+  const mesh = planeMesh(options)
   mesh.rotateX(-Math.PI / 2)
   return mesh
 }
